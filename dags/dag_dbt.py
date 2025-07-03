85c70d30cca13cf0c047e4d0eb2e09f1733e868b
diogominoru <diogominoru@hotmail.com>
2025-07-03 20:38:51 -0300

    criando dag job dbt cloud

---
+++ dags/dag_dbt.py
@@ -0,0 +1,27 @@
+from airflow.decorators import task, dag
+from airflow.providers.dbt.cloud.hooks.dbt import DbtCloudHook, DbtCloudJobRunStatus
+from airflow.providers.dbt.cloud.operators.dbt import DbtCloudRunJobOperator
+from datetime import datetime
+
+DBT_CLOUD_CONN_ID = "dbt"
+JOB_ID = "70471823480926"
+
+
+@dag(
+    start_date=datetime(2025, 7, 2),
+    schedule='@daily',
+    catchup=False
+)
+
+def run_dbt_cloud():
+    rodar_dbt = DbtCloudRunJobOperator(
+        job_name="dbt",
+        dbt_cloud_conn_id=DBT_CLOUD_CONN_ID,
+        job_id=JOB_ID,
+        check_interval=60,
+        timeout=360
+    )
+    
+    rodar_dbt
+    
+run_dbt_cloud()
